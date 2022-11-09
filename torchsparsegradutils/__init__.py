abd4d5257583f2f11f9efe401b191a4201cb7eb4
theo-barfoot <theodore.d.barfoot@kcl.ac.uk>
2022-11-09 14:48:25 +0000

    module init fixed

--- torchsparsegradutils/__init__.py
+++ torchsparsegradutils/__init__.py
@@ -1,4 +1,4 @@
-from sparse_matmul import sparse_mm
-from sparse_solve import sparse_triangular_solve
+from .sparse_matmul import sparse_mm
+from .sparse_solve import sparse_triangular_solve
 
-__all__ ['sparse_mm', 'sparse_triangular_solve']
+__all__ = ['sparse_mm', 'sparse_triangular_solve']
