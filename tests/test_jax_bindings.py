8310e73cb79f2ddb2b4b624c6d530ed5af30a16c
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2022-11-13 18:49:40 +0000

    basic support for wrapping a pytorch spare matrix as a jax linear operator

--- tests/test_jax_bindings.py
+++ tests/test_jax_bindings.py
@@ -20,10 +20,15 @@ class J2TIOTest(unittest.TestCase):
         if not hasattr(self, "device"):
             self.device = torch.device("cpu")
             self.device_j = jax.devices("cpu")[0]
-        self.x_shape = (4, 4)
+        self.x_shape = (4, 2)
         self.x_t = torch.randn(self.x_shape, dtype=torch.float64, device=self.device)
-        self.x_j = jax.device_put(np.random.randn(self.x_shape[0], self.x_shape[1]), device=self.device_j)
-        # print(self.x_j.device_buffer.device())
+        rng = np.random.default_rng()
+        x_n = rng.standard_normal(self.x_shape, dtype=np.float64)
+        # Note that by default jax does not support 64bit precision
+        # https://jax.readthedocs.io/en/latest/notebooks/Common_Gotchas_in_JAX.html#double-64bit-precision
+        self.x_j = jax.device_put(x_n, device=self.device_j)
+        self.A_shape = (4, 4)
+        self.A = torch.randn(self.A_shape, dtype=torch.float64, device=self.device).relu().to_sparse_csr()
 
     def test_t2j(self):
         x_j = tsgujax.t2j(self.x_t)
@@ -40,6 +45,18 @@ class J2TIOTest(unittest.TestCase):
         x_j2t2j = tsgujax.t2j(x_t)
         self.assertTrue(x_j2t2j.shape == self.x_j.shape)
         self.assertTrue(np.isclose(np.asarray(x_j2t2j), np.asarray(self.x_j)).all())
+
+    def test_spmm_t4j(self):
+        if jax.config.jax_enable_x64:
+            A_t = self.A
+        else:
+            A_t = self.A.to(torch.float32)
+        A_j = tsgujax.spmm_t4j(A_t)
+        Ax_j = A_j(self.x_j)
+        x_t = tsgujax.j2t(self.x_j)
+        Ax_t = A_t @ x_t
+        self.assertTrue(Ax_j.shape == Ax_t.shape)
+        self.assertTrue(np.isclose(np.asarray(Ax_j), Ax_t.numpy()).all())
 
 
 class J2TIOTestCUDA(J2TIOTest):
