a7931c4ad1f98f12132e5a0a8c51b81e3fe730e7
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2022-11-22 21:52:15 +0000

    basic IO bindings for cupy - see #9

---
+++ torchsparsegradutils/cupy/__init__.py
@@ -0,0 +1,16 @@
+import importlib
+
+cupy_spec = importlib.util.find_spec("cupy")
+if cupy_spec is None:
+    have_cupy = False
+    import warnings
+
+    warnings.warn(
+        "\n\nAttempting to import an optional module in torchsparsegradutils that depends on cupy but cupy couldn't be imported -> falling back to numpy.\n"
+    )
+else:
+    have_cupy = True
+
+from .cupy_bindings import c2t_coo, t2c_coo, c2t_csr, t2c_csr
+
+__all__ = ["c2t_coo", "t2c_coo", "c2t_csr", "t2c_csr"]
