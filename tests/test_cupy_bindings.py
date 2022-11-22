a347045f1c339d02942732c20c5bf3449b1f5035
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2022-11-22 21:52:15 +0000

    basic IO bindings for cupy - see #9

---
+++ tests/test_cupy_bindings.py
@@ -0,0 +1,89 @@
+import torch
+import unittest
+import torchsparsegradutils as tsgu
+import torchsparsegradutils.cupy as tsgucupy
+
+import warnings
+
+if tsgucupy.have_cupy:
+    import cupy as cp
+    import cupyx.scipy.sparse as csp
+else:
+    import scipy.sparse as nsp
+    warnings.warn("Importing optional cupy-related module failed to find cupy -> cupy-related tests running as numpy only.")
+
+import numpy as np
+
+
+
+
+class C2TIOTest(unittest.TestCase):
+    """IO conversion tests between torch and cupy"""
+
+    def setUp(self) -> None:
+        # The device can be specialised by a daughter class
+        if not hasattr(self, "device"):
+            self.device = torch.device("cpu")
+            self.xp = np
+            self.xsp = nsp
+        
+        self.x_shape = (4, 2)
+        self.x_t = torch.randn(self.x_shape, dtype=torch.float64, device=self.device)
+        rng = np.random.default_rng()
+        x_n = rng.standard_normal(self.x_shape, dtype=np.float64)
+
+        self.x_c = self.xp.asarray( x_n )
+        self.A_shape = (4, 4)
+        self.A = torch.randn(self.A_shape, dtype=torch.float64, device=self.device).relu().to_sparse_csr()
+
+    def test_t2c_coo(self):
+        x_t_coo = self.x_t.to_sparse_coo()
+        x_c_coo = tsgucupy.t2c_coo(x_t_coo)
+        self.assertTrue(x_c_coo.shape == x_t_coo.shape)
+        self.assertTrue(np.isclose(np.asarray(x_c_coo.todense()), x_t_coo.to_dense().numpy()).all())
+        x_t2c2t_coo = tsgucupy.c2t_coo(x_c_coo)
+        self.assertTrue(x_t2c2t_coo.shape == x_t_coo.shape)
+        self.assertTrue(np.isclose(x_t2c2t_coo.to_dense().numpy(), x_t_coo.to_dense().numpy()).all())
+
+    def test_c2t_coo(self):
+        x_c_coo = self.xsp.coo_matrix(self.x_c)
+        x_t_coo = tsgucupy.c2t_coo(x_c_coo)
+        self.assertTrue(x_c_coo.shape == x_t_coo.shape)
+        self.assertTrue(np.isclose(np.asarray(x_c_coo.todense()), x_t_coo.to_dense().numpy()).all())
+        x_c2t2c_coo = tsgucupy.t2c_coo(x_t_coo)
+        self.assertTrue(x_c2t2c_coo.shape == x_c_coo.shape)
+        self.assertTrue(np.isclose(np.asarray(x_c2t2c_coo.todense()), np.asarray(x_c_coo.todense())).all())
+
+    def test_t2c_csr(self):
+        x_t_csr = self.x_t.to_sparse_csr()
+        x_c_csr = tsgucupy.t2c_csr(x_t_csr)
+        self.assertTrue(x_c_csr.shape == x_t_csr.shape)
+        self.assertTrue(np.isclose(np.asarray(x_c_csr.todense()), x_t_csr.to_dense().numpy()).all())
+        x_t2c2t_csr = tsgucupy.c2t_csr(x_c_csr)
+        self.assertTrue(x_t2c2t_csr.shape == x_t_csr.shape)
+        self.assertTrue(np.isclose(x_t2c2t_csr.to_dense().numpy(), x_t_csr.to_dense().numpy()).all())
+
+    def test_c2t_csr(self):
+        x_c_csr = self.xsp.csr_matrix(self.x_c)
+        x_t_csr = tsgucupy.c2t_csr(x_c_csr)
+        self.assertTrue(x_c_csr.shape == x_t_csr.shape)
+        self.assertTrue(np.isclose(np.asarray(x_c_csr.todense()), x_t_csr.to_dense().numpy()).all())
+        x_c2t2c_csr = tsgucupy.t2c_csr(x_t_csr)
+        self.assertTrue(x_c2t2c_csr.shape == x_c_csr.shape)
+        self.assertTrue(np.isclose(np.asarray(x_c2t2c_csr.todense()), np.asarray(x_c_csr.todense())).all())
+
+
+class C2TIOTestCUDA(C2TIOTest):
+    """Override superclass setUp to run on GPU"""
+
+    def setUp(self) -> None:
+        if not torch.cuda.is_available():
+            self.skipTest(f"Skipping {self.__class__.__name__} since CUDA is not available")
+        self.device = torch.device("cuda")
+        self.xp = cp
+        self.xsp = csp
+        super().setUp()
+        
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ torchsparsegradutils/cupy/cupy_bindings.py
@@ -0,0 +1,70 @@
+import torchsparsegradutils.cupy as tsgucupy
+if tsgucupy.have_cupy:
+    import cupy as cp
+    import cupyx.scipy.sparse as csp
+else:
+    import numpy as np
+    import scipy.sparse as nsp
+
+import torch
+
+import warnings
+
+def _get_array_modules(x):
+    # Get cupy or numpy based on context
+    if tsgucupy.have_cupy:
+        if torch.is_tensor(x):
+            if x.device == torch.device("cpu"):
+                xp = np
+                xsp = nsp
+            else:
+                xp = cp
+                xsp = csp
+        else:
+            xp = cp.get_array_module(x_cupy)
+            xsp = cupyx.scipy.get_array_module(x_cupy)
+    else:
+        xp = np
+        xsp = nsp
+    return xp, xsp
+
+def t2c_csr(x_torch):
+    # Convert a torch CSR tensor to a cupy/numpy CSR array
+    xp, xsp = _get_array_modules(x_torch)
+    
+    data_c = xp.asarray(x_torch.values())
+    col_idx_c = xp.asarray(x_torch.col_indices())
+    ind_ptr_c = xp.asarray(x_torch.crow_indices())
+    x_cupy = xsp.csr_matrix((data_c, col_idx_c, ind_ptr_c))
+    return x_cupy
+
+
+def c2t_csr(x_cupy):
+    # Convert a cupy/numpy CSR array to a torch CSR tensor
+    data_t = torch.as_tensor(x_cupy.data)
+    idices_t = torch.as_tensor(x_cupy.indices)
+    ind_ptr_t = torch.as_tensor(x_cupy.indptr)
+    x_torch = torch.sparse_csr_tensor(ind_ptr_t, idices_t, data_t, x_cupy.shape)
+    return x_torch
+
+
+def t2c_coo(x_torch):
+    # Convert a torch COO tensor to a cupy/numpy COO array
+    xp, xsp = _get_array_modules(x_torch)
+    
+    if not x_torch.is_coalesced():
+        warnings.warn("Requested a conversion from torch to cupy/numpy on a non-coalesced tensor -> coalescing implicitly")
+        x_torch = x_torch.coalesce()
+    data_c = xp.asarray(x_torch.values())
+    idx_cp = xp.asarray(x_torch.indices())
+    x_cupy = xsp.coo_matrix((data_c, idx_cp))
+    return x_cupy
+
+
+def c2t_coo(x_cupy):
+    # Convert a cupy COO array to a torch COO tensor
+    data_t = torch.as_tensor(x_cupy.data)
+    row_t = torch.as_tensor(x_cupy.row)
+    col_t = torch.as_tensor(x_cupy.col)
+    x_torch = torch.sparse_coo_tensor(torch.stack([row_t, col_t], dim=0), data_t, x_cupy.shape)
+    return x_torch
