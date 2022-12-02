f5e7aeae3ad8cf78ae150b97c80681e4b560b7b6
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2022-12-02 20:50:15 +0000

    adapted bicgstab from pykrilov for torchsparsegradutils

--- torchsparsegradutils/utils/__init__.py
+++ torchsparsegradutils/utils/__init__.py
@@ -1,4 +1,5 @@
 from .linear_cg import linear_cg, LinearCGSettings
 from .minres import minres, MINRESSettings
+from .bicgstab import bicgstab, BICGSTABSettings
 
-__all__ = ["linear_cg", "LinearCGSettings", "minres", "MINRESSettings"]
+__all__ = ["linear_cg", "LinearCGSettings", "minres", "MINRESSettings", "bicgstab", "BICGSTABSettings"]
