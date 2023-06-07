61f90d618090fa2cd1353a8cd91679c8655ca773
theo-barfoot <theo.barfoot@gmail.com>
2023-06-07 16:01:24 +0100

    autogen tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import torch
 import unittest
+from unittest.mock import Mock
+
 from parameterized import parameterized_class, parameterized
 from torchsparsegradutils.utils.random_sparse import (
     generate_random_sparse_coo_matrix,
@@ -242,3 +244,118 @@ class TestSparseBlockDiag(unittest.TestCase):
         Ad_block_diag = torch.block_diag(*A_d)
         self.assertTrue(torch.equal(A_csr_block_diag.to_dense(), Ad_block_diag))
         # TODO: sparse_block_diag changes (in place) the indices of the input CSR tensor
+
+    def test_no_arguments(self):
+        with self.assertRaises(ValueError):
+            sparse_block_diag()
+
+    def test_incorrect_tensor_layout(self):
+        coo_tensor = Mock(spec=torch.Tensor)
+        coo_tensor.layout = torch.sparse_coo
+        csr_tensor = Mock(spec=torch.Tensor)
+        csr_tensor.layout = torch.sparse_csr
+        with self.assertRaises(ValueError):
+            sparse_block_diag(coo_tensor, csr_tensor)
+
+    def test_incorrect_sparse_dim(self):
+        coo_tensor = Mock(spec=torch.Tensor)
+        coo_tensor.layout = torch.sparse_coo
+        coo_tensor.sparse_dim.return_value = 1
+        with self.assertRaises(ValueError):
+            sparse_block_diag(coo_tensor)
+
+    def test_incorrect_dense_dim(self):
+        coo_tensor = Mock(spec=torch.Tensor)
+        coo_tensor.layout = torch.sparse_coo
+        coo_tensor.dense_dim.return_value = 1
+        with self.assertRaises(ValueError):
+            sparse_block_diag(coo_tensor)
+
+    def test_incorrect_input_type(self):
+        with self.assertRaises(TypeError):
+            sparse_block_diag("Not a list or tuple")
+
+    def test_incorrect_tensor_type_in_list(self):
+        tensor1 = torch.randn(5, 5).to_sparse().to(device=self.device)
+        tensor2 = "Not a tensor"
+        with self.assertRaises(TypeError):
+            sparse_block_diag(tensor1, tensor2)
+
+    def test_different_shapes_coo(self):
+        tensor1 = torch.randn(5, 5).to_sparse_coo().to(device=self.device)
+        tensor2 = torch.randn(3, 3).to_sparse_coo().to(device=self.device)
+        tensor3 = torch.randn(2, 2).to_sparse_coo().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2, tensor3)
+        self.assertEqual(result.shape, torch.Size([10, 10]))
+
+    def test_different_shapes_csr(self):
+        tensor1 = torch.randn(5, 5).to_sparse_csr().to(device=self.device)
+        tensor2 = torch.randn(3, 3).to_sparse_csr().to(device=self.device)
+        tensor3 = torch.randn(2, 2).to_sparse_csr().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2, tensor3)
+        self.assertEqual(result.shape, torch.Size([10, 10]))
+
+    def test_too_many_tensor_dimensions(self):
+        tensor1 = torch.randn(5, 5, 5).to_sparse().to(device=self.device)
+        with self.assertRaises(ValueError):
+            sparse_block_diag(tensor1)
+
+    def test_empty_tensor_coo(self):
+        tensor1 = torch.sparse_coo_tensor(torch.empty([2, 0]), torch.empty([0])).to(device=self.device)
+        result = sparse_block_diag(tensor1)
+        self.assertEqual(result.shape, torch.Size([0, 0]))
+
+    def test_empty_tensor_coo_mix(self):
+        tensor1 = torch.empty((0, 0)).to_sparse_coo().to(device=self.device)
+        tensor2 = torch.randn(3, 3).to_sparse_coo().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2)
+        expected = tensor2.to_dense()
+        self.assertTrue(torch.equal(result.to_dense(), expected))
+
+    def test_empty_tensor_csr(self):
+        tensor1 = torch.sparse_csr_tensor(torch.empty([2, 0]), torch.empty([0])).to(device=self.device)
+        result = sparse_block_diag(tensor1)
+        self.assertEqual(result.shape, torch.Size([0, 0]))
+
+    def test_empty_tensor_csr_mix(self):
+        tensor1 = torch.empty((0, 0)).to_sparse_csr().to(device=self.device)
+        tensor2 = torch.randn(3, 3).to_sparse_csr().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2)
+        expected = tensor2.to_dense()
+        self.assertTrue(torch.equal(result.to_dense(), expected))
+
+    def test_single_tensor_coo(self):
+        tensor1 = torch.randn(5, 5).to_sparse_coo().to(device=self.device)
+        result = sparse_block_diag(tensor1)
+        self.assertTrue(torch.equal(result.to_dense(), tensor1.to_dense()))
+
+    def test_single_tensor_csr(self):
+        tensor1 = torch.randn(5, 5).to_sparse_csr().to(device=self.device)
+        result = sparse_block_diag(tensor1)
+        self.assertTrue(torch.equal(result.to_dense(), tensor1.to_dense()))
+
+    def test_zero_tensor_coo(self):
+        tensor1 = torch.zeros(5, 5).to_sparse().to(device=self.device)
+        tensor2 = torch.zeros(3, 3).to_sparse().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2)
+        self.assertEqual(result.shape, torch.Size([8, 8]))
+        self.assertTrue((result.to_dense() == 0).all())
+
+    def test_zero_tensor_csr(self):
+        tensor1 = torch.zeros(5, 5).to_sparse_csr().to(device=self.device)
+        tensor2 = torch.zeros(3, 3).to_sparse_csr().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2)
+        expected = torch.zeros(8, 8).to(device=self.device)
+        self.assertTrue(torch.equal(result.to_dense(), expected))
+
+    def test_non_square_tensor_coo(self):
+        tensor1 = torch.randn(5, 7).to_sparse().to(device=self.device)
+        tensor2 = torch.randn(3, 2).to_sparse().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2)
+        self.assertEqual(result.shape, torch.Size([8, 9]))
+
+    def test_non_square_tensor_csr(self):
+        tensor1 = torch.randn(5, 7).to_sparse_csr().to(device=self.device)
+        tensor2 = torch.randn(3, 2).to_sparse_csr().to(device=self.device)
+        result = sparse_block_diag(tensor1, tensor2)
+        self.assertEqual(result.shape, torch.Size([8, 9]))
