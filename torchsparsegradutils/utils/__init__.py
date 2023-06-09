a6f026d754f1063eb758d17e10ed4852947171c7
Theo Barfoot <theo.barfoot@gmail.com>
2023-06-09 19:05:02 +0100

    add utils and random

--- torchsparsegradutils/utils/__init__.py
+++ torchsparsegradutils/utils/__init__.py
@@ -2,5 +2,7 @@ from .linear_cg import linear_cg, LinearCGSettings
 from .minres import minres, MINRESSettings
 from .bicgstab import bicgstab, BICGSTABSettings
 from .lsmr import lsmr
+from .utils import convert_coo_to_csr_indices_values, convert_coo_to_csr, sparse_block_diag
+from .random_sparse import rand_sparse, rand_sparse_tri
 
-__all__ = ["linear_cg", "LinearCGSettings", "minres", "MINRESSettings", "bicgstab", "BICGSTABSettings", "lsmr"]
+__all__ = ["linear_cg", "LinearCGSettings", "minres", "MINRESSettings", "bicgstab", "BICGSTABSettings", "lsmr", "convert_coo_to_csr_indices_values", "convert_coo_to_csr", "sparse_block_diag", "rand_sparse", "rand_sparse_tri"]
