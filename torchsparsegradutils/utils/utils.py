67cb9cb6fd8b786f1761c9737377953d33309e12
Theo Barfoot <theo.barfoot@gmail.com>
2023-06-13 18:48:22 +0100

    add csr stack and block diag methods

--- torchsparsegradutils/utils/utils.py
+++ torchsparsegradutils/utils/utils.py
@@ -1,5 +1,42 @@
 import torch
 
+def stack_csr(tensors, dim=0):
+    """
+    Stacks a list of CSR tensors along the batch dimension.
+    This function is analogous to torch.stack() but for CSR tensors.
+
+    Args:
+        tensors (list): List of CSR tensors to be stacked.
+        dim (int): The axis to stack the tensors along. Default is 0.
+
+    Returns:
+        torch.Tensor: Stacked CSR tensor.
+    """
+    if not isinstance(tensors, (list, tuple)):
+        raise TypeError("Expected a list of tensors, but got {}.".format(type(tensors)))
+    
+    if len(tensors) == 0:
+        raise ValueError("Cannot stack empty list of tensors.")
+
+    if not all([tensor.shape == tensors[0].shape for tensor in tensors]):
+        raise ValueError("All tensors must have the same shape.")
+
+    if not all([tensor.layout == torch.sparse_csr for tensor in tensors]):
+        raise ValueError("All tensors must be CSR.")
+
+    if not all([tensor.ndim == 2 for tensor in tensors]):
+        raise ValueError("All tensors must be 2D.")
+    
+    crow_indices = torch.stack([tensor.crow_indices() for tensor in tensors], dim=dim)
+    col_indices = torch.stack([tensor.col_indices() for tensor in tensors], dim=dim)
+    values = torch.stack([tensor.values() for tensor in tensors], dim=dim)
+
+    shape = list(tensors[0].shape)
+    shape.insert(dim, len(tensors))
+    shape = tuple(shape)
+
+    return torch.sparse_csr_tensor(crow_indices, col_indices, values, shape)
+
 
 def _sort_coo_indices(indices):
     """
@@ -143,3 +180,218 @@ def _demcompress_crow_indices(crow_indices, num_rows):
     )
 
     return row_indices
+
+
+# use @torch.jit.script ?
+def sparse_block_diag(*sparse_tensors):
+    """
+    Function to create a block diagonal sparse matrix from provided sparse tensors.
+    This function is designed to replicate torch.block_diag(), but for sparse tensors,
+    but only supports 2D sparse tensors 
+    (whereas torch.block_diag() supports dense tensors of 0, 1 or 2 dimensions).
+
+    Args:
+        *sparse_tensors (torch.Tensor): Variable length list of sparse tensors. All input tensors must either be all 
+                                        sparse_coo or all sparse_csr format. The input sparse tensors must have exactly 
+                                        two sparse dimensions and no dense dimensions.
+
+    Returns:
+        A block diagonal sparse tensor in the same format (either sparse_coo or sparse_csr) as the input tensors.
+
+    Raises:
+        TypeError: If the inputs are not provided as a list or tuple.
+        ValueError: If no tensors are provided or if the provided tensors are not all of the same sparse format.
+        ValueError: If all sparse tensors do not have two sparse dimensions and zero dense dimensions.
+    """
+
+    for i, sparse_tensor in enumerate(sparse_tensors):
+        if not isinstance(sparse_tensor, torch.Tensor):
+            raise TypeError(f"TypeError: expected Tensor as element {i} in argument 0, but got {type(sparse_tensor).__name__}")
+
+    if len(sparse_tensors) == 0:
+        raise ValueError("At least one sparse tensor must be provided.")
+    
+    if all(sparse_tensor.layout == torch.sparse_coo for sparse_tensor in sparse_tensors):
+        layout = torch.sparse_coo
+    elif all(sparse_tensor.layout == torch.sparse_csr for sparse_tensor in sparse_tensors):
+        layout = torch.sparse_csr
+    else:
+        raise ValueError("Sparse tensors must either be all sparse_coo or all sparse_csr.")
+    
+    if not all(sparse_tensor.sparse_dim() == 2 for sparse_tensor in sparse_tensors):
+        raise ValueError("All sparse tensors must have two sparse dimensions.")
+    
+    if not all(sparse_tensor.dense_dim() == 0 for sparse_tensor in sparse_tensors):
+        raise ValueError("All sparse tensors must have zero dense dimensions.")
+
+    if len(sparse_tensors) == 1:
+        return sparse_tensors[0]
+    
+    if layout == torch.sparse_coo:
+        
+        row_indices_list = []
+        col_indices_list = []
+        values_list = []
+
+        num_row = 0
+        num_col = 0
+        
+        for i, sparse_tensor in enumerate(sparse_tensors):            
+            
+            sparse_tensor = sparse_tensor.coalesce() if not sparse_tensor.is_coalesced() else sparse_tensor
+            
+            row_indices, col_indices = sparse_tensor.indices()
+            
+            # calculate block offsets
+            # not in-place addition to avoid modifying the original tensor indices
+            row_indices = row_indices + i * sparse_tensor.size()[-2]
+            col_indices = col_indices + i * sparse_tensor.size()[-1]
+
+            # accumulate indices and values:
+            row_indices_list.append(row_indices)
+            col_indices_list.append(col_indices)
+            values_list.append(sparse_tensor.values())
+
+            # accumulate tensor sizes:
+            num_row += sparse_tensor.size()[-2]
+            num_col += sparse_tensor.size()[-1]
+
+            
+        row_indices = torch.cat(row_indices_list)
+        col_indices = torch.cat(col_indices_list)
+        values = torch.cat(values_list)
+
+        return torch.sparse_coo_tensor(torch.stack([row_indices, col_indices]), values, torch.Size([num_row, num_col]))
+    
+    elif layout == torch.sparse_csr:
+        
+        crow_indices_list = []
+        col_indices_list = []
+        values_list = []
+
+        num_row = 0
+        num_col = 0
+        
+        for i, sparse_tensor in enumerate(sparse_tensors):
+            
+            crow_indices = sparse_tensor.crow_indices()
+            col_indices = sparse_tensor.col_indices()
+            
+            # Calculate block offsets
+            # not in-place addition to avoid modifying the original tensor indices
+            if i > 0:
+                crow_indices = crow_indices[1:]
+                crow_indices = crow_indices + crow_indices_list[-1][-1]
+            col_indices = col_indices + i * sparse_tensor.size()[-1]
+
+            # accumulate tensor sizes:
+            num_row += sparse_tensor.size()[-2]
+            num_col += sparse_tensor.size()[-1]
+
+            # Accumulate indices and values:
+            crow_indices_list.append(crow_indices)
+            col_indices_list.append(col_indices)
+            values_list.append(sparse_tensor.values())
+
+        crow_indices = torch.cat(crow_indices_list)
+        col_indices = torch.cat(col_indices_list)
+        values = torch.cat(values_list)
+
+        return torch.sparse_csr_tensor(crow_indices, col_indices, values, torch.Size([num_row, num_col]))
+    
+    
+def sparse_block_diag_split(sparse_block_diag_tensor, *shapes):
+    """
+    Function to split a block diagonal sparse matrix into original sparse tensors.
+    
+    NOTE: Sparse COO tensors are assumed to already by coalesced. 
+    This is because newly created or indexed sparse COO tensors default to is_coalesced=False,
+    and running coalesce() imposes an unnecessary performance penalty.
+
+    Args:
+        sparse_block_diag_tensor (torch.Tensor): The input block diagonal sparse tensor.
+        *shapes (sequence of tuple): The shapes of the original tensors. This is required to correctly split the tensor.
+
+    Returns:
+        A list of original sparse tensors.
+
+    Raises:
+        TypeError: If the input tensor is not a sparse tensor.
+    """
+
+    if sparse_block_diag_tensor.layout == torch.sparse_coo:
+        layout = torch.sparse_coo
+    elif sparse_block_diag_tensor.layout == torch.sparse_csr:
+        layout = torch.sparse_csr
+    else:
+        raise ValueError("Input tensor format not supported. Only sparse_coo and sparse_csr are supported.")
+    
+    if not all(len(shape) == 2 for shape in shapes):
+        raise ValueError("All shapes must be two-dimensional.")
+        
+    if layout == torch.sparse_coo:
+        tensors = []
+        start_row = 0
+        start_col = 0
+        current_val_offset = 0
+
+        sparse_block_diag_tensor = sparse_block_diag_tensor.coalesce() if not sparse_block_diag_tensor.is_coalesced() else sparse_block_diag_tensor
+        
+        row_indices, col_indices = sparse_block_diag_tensor.indices()
+        values = sparse_block_diag_tensor.values()
+
+        for shape in shapes:
+            rows, cols = shape[-2], shape[-1]
+
+            mask_row = (start_row <= row_indices) & (row_indices < start_row + rows)
+            mask_col = (start_col <= col_indices) & (col_indices < start_col + cols)
+            mask = mask_row & mask_col
+
+            indices_sub = torch.stack((row_indices[mask] - start_row, col_indices[mask] - start_col))
+            values_sub = values[mask]
+
+            tensor_sub = torch.sparse_coo_tensor(indices_sub, values_sub, (rows, cols))
+            tensors.append(tensor_sub)
+
+            start_row += rows
+            start_col += cols
+            current_val_offset += rows * cols
+
+        return tuple(tensors)
+
+    elif layout == torch.sparse_csr:
+        tensors = []
+        start_row = 0
+        current_col_offset = 0
+        current_val_offset = 0
+        
+        crow_indices = sparse_block_diag_tensor.crow_indices()
+        col_indices = sparse_block_diag_tensor.col_indices()
+        values = sparse_block_diag_tensor.values()
+
+        for shape in shapes:
+            rows, cols = shape[-2], shape[-1]
+
+            # Compute the number of values in the sub-block
+            values_count = crow_indices[start_row + shape[0]] - crow_indices[start_row]
+
+            # Find the starting and ending points in crow_indices
+            start_ptr = crow_indices[start_row]
+            end_ptr = crow_indices[start_row + shape[0]]
+
+            # Apply the pointers to get the sub-block indices and values
+            col_indices_sub = col_indices[current_val_offset:current_val_offset+values_count] - current_col_offset
+            values_sub = values[current_val_offset:current_val_offset+values_count]
+
+            # Create the sub-block crow_indices
+            crow_indices_sub = crow_indices[start_row:start_row + shape[0] + 1] - start_ptr
+
+            # Construct the sub-block as a CSR tensor
+            tensor_sub = torch.sparse_csr_tensor(crow_indices_sub, col_indices_sub, values_sub, (rows, cols))
+
+            tensors.append(tensor_sub)
+            start_row += shape[0]
+            current_col_offset += cols
+            current_val_offset += values_count
+
+        return tuple(tensors)
