c448144bf124e3fd0e715a7a19212ea702a24934
Theo Barfoot <theo.barfoot@gmail.com>
2026-07-07 19:40:53 +0000

    Bump version to 0.2.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@ sys.path.insert(0, os.path.abspath("../../"))
 project = "torchsparsegradutils"
 copyright = "2026, CAI4CAI research group"
 author = "CAI4CAI research group"
-release = "0.2.2"
-version = "0.2.2"
+release = "0.2.3"
+version = "0.2.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
